60dd4577d453e1cb2e350e6a9ad23846d3d9c156
秧秧 <wavesyang@outlook.com>
2026-07-28 21:34:52 +0800

    添加视觉代码，来自ljz

---
+++ src/animal_tracker/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from glob import glob
+
+package_name = 'animal_tracker'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'animal_tracker_node = animal_tracker.animal_tracker_node:main',
+        ],
+    },
+)
